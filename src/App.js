6d23134b6585260d6ff3cbe64349d4bd6eeedd14
Saurav <alexthundrous2104@gmail.com>
2023-06-21 15:29:01 +0530

    src: Fetch user data using api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from "react";
-import { robots } from "../components/robots";
+// import { robots } from "../components/robots";
 import CardList from '../components/CardList';
 import Searchbox from "../components/Searchbox";
 import Scroll from "../components/Scroll";
-import style from "./App.css";
 
 class App extends React.Component  {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
   }
 }
 
+componentDidMount() {
+  fetch("https://jsonplaceholder.typicode.com/users").then( response => response.json).then(users => this.setState({robots: users}))
+}
+
   onSearchChange(event) {
-    this.setState({searchfield: event.target.value});
+    this.setState({searchfield: event.target.value}); // gives value of what is searched (target.value) setState is mandatory to change searchfield we always use this and not this.state.searchfield = ...
   }   
 
   render() {
   const filteredrobots = this.state.robots.filter(robot => {
-    return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()) //we use filtered robots in form of function since elsewise it takes this as the html input and not the react component
   })
   return(
     <div class="App">
