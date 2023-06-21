6a9a2719c076bd580cd5a769d02a9c6823cf5b0e
Saurav <alexthundrous2104@gmail.com>
2023-06-21 16:00:25 +0530

    Fix a bug

--- src/container/App.js
+++ src/container/App.js
@@ -1,31 +1,30 @@
 import React from "react";
-import { robots } from "../components/robots";
 import CardList from '../components/CardList';
 import Searchbox from "../components/Searchbox";
 import Scroll from "../components/Scroll";
-import style from "./App.css";
 
 class App extends React.Component  {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
   }
 }
 
+componentDidMount() {
+  fetch("https://jsonplaceholder.typicode.com/users").then( response => response.json).then(users => this.setState({robots: users}))
+}
+
   onSearchChange(event) {
-    this.setState({searchfield: event.target.value});
+    this.setState({searchfield: event.target.value}); // gives value of what is searched (target.value) setState is mandatory to change searchfield we always use this and not this.state.searchfield = ...
   }   
 
   render() {
-  const {robots, searchfield} = this.state;
-  const filteredrobots = robots.filter(robot => {
-    return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+  const filteredrobots = this.state.robots.filter(robot => {
+    return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()) //we use filtered robots in form of function since elsewise it takes this as the html input and not the react component
   })
-  return !robots.length === 0 ?
- <h1> Loading </h1> : 
- (
+  return(
     <div class="App">
        <h1> RoboFriends </h1>
        <Searchbox searchChange = {(event) => {this.onSearchChange(event)}}/>
@@ -37,6 +36,5 @@ class App extends React.Component  {
   );
 }
 }
-
 
 export default App;
