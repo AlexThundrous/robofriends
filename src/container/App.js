ce6e1b1ccde911f0e3393ba6bca005b17aeb2584
Saurav <alexthundrous2104@gmail.com>
2023-06-23 20:33:09 +0530

    Quick fix

--- src/container/App.js
+++ src/container/App.js
@@ -2,39 +2,44 @@ import React from "react";
 import CardList from '../components/CardList';
 import Searchbox from "../components/Searchbox";
 import Scroll from "../components/Scroll";
+import "./App.css";
 
-class App extends React.Component  {
+class App extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
       robots: [],
       searchfield: ''
+    }
   }
-}
 
-componentDidMount() {
-  fetch("https://jsonplaceholder.typicode.com/users").then( response => response.json).then(users => this.setState({robots: users}))
-}
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});
+  }
 
-  onSearchChange(event) {
-    this.setState({searchfield: event.target.value}); // gives value of what is searched (target.value) setState is mandatory to change searchfield we always use this and not this.state.searchfield = ...
-  }   
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
 
   render() {
-  const filteredrobots = this.state.robots.filter(robot => {
-    return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()) //we use filtered robots in form of function since elsewise it takes this as the html input and not the react component
-  })
-  return(
-    <div class="App">
-       <h1> RoboFriends </h1>
-       <Searchbox searchChange = {(event) => {this.onSearchChange(event)}}/>
-       <br />
-       <Scroll>
-       <CardList robots = {filteredrobots} />
-       </Scroll>
-    </div>
-  );
-}
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
+        <div className="App">
+          <h1>RoboFriends</h1>
+          <Searchbox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+  }
 }
 
 export default App;
